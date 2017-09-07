12b54b87f0884b98487701b92473b48e0fefc25a
10ngawang <tenngawang6080@gmail.com>
2017-09-06 21:49:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/alltibetan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alltibetan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
