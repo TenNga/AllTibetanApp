489e4bc2022bbb8f266f1b467a51ac7397cb9615
10ngawang <tenngawang6080@gmail.com>
2017-09-08 15:28:49 -0400

    Fixed layout and added menu in middle

--- app/src/main/java/com/example/alltibetan/MainActivity.java
+++ app/src/main/java/com/example/alltibetan/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alltibetan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gotoNextpage(View view){
+        Intent nextPage = new Intent(MainActivity.this, option2.class);
+        startActivity(nextPage);
+    }
+
 }
